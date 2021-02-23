8401df8ddeecf319080bcd8434d280dad848f299
psx <96hitpsx@gmail.com>
2021-02-23 16:41:40 +0800

    首次提交代码

---
+++ src/main/java/com/psx/service/threadStudy.java
@@ -0,0 +1,21 @@
+package com.psx.service;
+
+
+class threadClass extends Thread {
+    String name;
+    threadClass(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("线程" + this.name + "执行" + i);
+        }
+    }
+}
+public class threadStudy {
+    public static void main(String[] args) {
+        threadClass t = new threadClass("hello");
+        t.start();
+    }
+}
