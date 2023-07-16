b54f7537048784a48eeb7c4fe982e5623a091473
hitpsx <96hitpsx@gmail.com>
2023-07-16 23:12:07 +0800

    首次测试

--- src/main/java/com/psx/service/thread/simpleTread/noReturn/runableStudy.java
+++ src/main/java/com/psx/service/thread/simpleTread/noReturn/runableStudy.java
@@ -1,17 +1,19 @@
 package com.psx.service.thread.simpleTread.noReturn;
 
-class runableClass implements Runnable{
+class runableClass implements Runnable {
     String name;
 
     runableClass(String name) {
         this.name = name;
     }
-    public  void  run() {
+
+    public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println("线程" + this.name + "执行" + i);
         }
     }
 }
+//首次提交
 
 public class runableStudy {
     public static void main(String[] args) {
